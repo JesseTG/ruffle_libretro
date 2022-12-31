0dd785575cc432646f6ce95c2d3b238de9f15311
Jesse Talavera-Greenberg <jesse@jesse.tg>
2022-12-31 12:44:49 -0500

    Add some enums

--- src/core/state.rs
+++ src/core/state.rs
@@ -2,9 +2,19 @@ use std::cell::Cell;
 use std::sync::{Arc, Mutex};
 
 use ruffle_core::{Player, PlayerBuilder};
+use rust_libretro_sys::*;
 
 pub enum PlayerState {
     Uninitialized,
     Pending(Cell<PlayerBuilder>),
     Active(Arc<Mutex<Player>>),
+}
+
+pub enum RenderInterface {
+    Default(retro_hw_render_interface),
+    Vulkan(retro_hw_render_interface_vulkan),
+}
+
+pub enum RenderContextNegotiationInterface {
+    Vulkan(retro_hw_render_context_negotiation_interface_vulkan)
 }
