fba7d41365c2150e764908527999c462a264d417
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-01-04 20:36:34 -0500

    Select both queue families

--- src/backend/render/vulkan/negotiation.rs
+++ src/backend/render/vulkan/negotiation.rs
@@ -5,25 +5,24 @@ use std::ptr;
 use std::slice::from_raw_parts;
 use std::sync::Once;
 
-use ash::{Entry, Instance, vk};
 use ash::extensions::khr::Surface;
 use ash::prelude::VkResult;
 use ash::vk::{
-    ApplicationInfo, ExtensionProperties, LayerProperties,
-    PFN_vkGetInstanceProcAddr, PhysicalDevice, PhysicalDeviceFeatures, PhysicalDeviceProperties, QueueFamilyProperties,
-    QueueFlags, StaticFn, SurfaceKHR,
+    ApplicationInfo, ExtensionProperties, LayerProperties, PFN_vkGetInstanceProcAddr, PhysicalDevice,
+    PhysicalDeviceFeatures, PhysicalDeviceProperties, QueueFamilyProperties, QueueFlags, StaticFn, SurfaceKHR,
 };
+use ash::{vk, Entry, Instance};
 use log::{debug, error, info, log, warn};
+use rust_libretro_sys::retro_hw_render_context_negotiation_interface_type::RETRO_HW_RENDER_CONTEXT_NEGOTIATION_INTERFACE_VULKAN;
 use rust_libretro_sys::{
     retro_hw_render_context_negotiation_interface_type, retro_hw_render_context_negotiation_interface_vulkan,
-    RETRO_HW_RENDER_CONTEXT_NEGOTIATION_INTERFACE_VULKAN_VERSION, retro_vulkan_context,
+    retro_vulkan_context, RETRO_HW_RENDER_CONTEXT_NEGOTIATION_INTERFACE_VULKAN_VERSION,
 };
-use rust_libretro_sys::retro_hw_render_context_negotiation_interface_type::RETRO_HW_RENDER_CONTEXT_NEGOTIATION_INTERFACE_VULKAN;
 use tap::pipe::Pipe;
 use thiserror::Error as ThisError;
 
-use crate::backend::render::HardwareRenderContextNegotiationInterface;
 use crate::backend::render::vulkan::negotiation::VulkanNegotiationError::*;
+use crate::backend::render::HardwareRenderContextNegotiationInterface;
 use crate::built_info;
 
 #[derive(ThisError, Debug)]
@@ -176,13 +175,13 @@ impl VulkanContextNegotiationInterface {
          * The frontend will request certain extensions and layers for a device which is created.
          * The core must ensure that the queue and queue_family_index support GRAPHICS and COMPUTE.
          */
-        let surface_khr = if surface == SurfaceKHR::null() {
+        let surface_fn = if surface == SurfaceKHR::null() {
             None
         } else {
             Some(Surface::new(&entry, &instance))
         };
 
-        let surface = if surface_khr.is_none() { None } else { Some(surface) };
+        let surface = if surface_fn.is_none() { None } else { Some(surface) };
 
         let mut interface = INSTANCE.as_mut().unwrap();
         interface.entry = Some(entry);
@@ -234,23 +233,32 @@ impl VulkanContextNegotiationInterface {
             }
         };
 
-        // get the first index into device.queue_families which supports graphics and compute
-        let (queue_family_index, queue_family_properties) = match Self::select_graphics_queue_family(&physical_device) {
-            Ok((index, props)) => {
-                info!("Selected queue family #{index}");
-                (index as u32, props)
+        let queue_families = if let (Some(surface), Some(surface_fn)) = (surface, surface_fn.as_ref()) {
+            // If VK_KHR_surface is enabled and the surface is valid...
+            match Self::select_queue_families(&physical_device, &surface_fn, surface) {
+                Ok(families) => families,
+                Err(error) => {
+                    error!("Error when searching for usable compute/graphics/present families: {error:?}");
+                    return false;
+                }
             }
-            Err(error) => {
-                error!("Error when selecting a queue family: {error:?}");
-                return false;
+        } else {
+            // Just get a queue with COMPUTE and GRAPHICS, then
+            match Self::select_queue_family(&physical_device) {
+                Ok(family) => QueueFamilies::new(family, family),
+                Err(error) => {
+                    error!("Error when searching for usable compute/graphics family: {error:?}");
+                    return false;
+                }
             }
         };
 
         (*context).gpu = physical_device.device;
-        (*context).queue_family_index = queue_family_index;
+        (*context).queue_family_index = queue_families.queue_family_index;
+        (*context).presentation_queue_family_index = queue_families.presentation_queue_family_index;
 
         interface.instance = Some(instance);
-        interface.surface_fns = surface_khr;
+        interface.surface_fns = surface_fn;
 
         todo!()
     }
@@ -391,43 +399,86 @@ impl VulkanContextNegotiationInterface {
         features.iter().sum::<vk::Bool32>() > 0
     }
 
-    fn select_graphics_queue_family(
-        physical_device: &PhysicalDeviceInfo,
-    ) -> Result<(usize, &QueueFamilyProperties), VulkanNegotiationError> {
+    // Only used if the surface extension isn't available
+    // (Probably means we're not rendering to the screen)
+    fn select_queue_family(physical_device: &PhysicalDeviceInfo) -> Result<u32, VulkanNegotiationError> {
         // The core must ensure that the queue and queue_family_index support GRAPHICS and COMPUTE.
         physical_device
             .queue_families
             .iter()
             .enumerate()
-            .find(|(i, family)| {
+            .find_map(|(i, family)| {
                 // Get the first queue family that supports the features we need.
-                family.queue_flags.contains(QueueFlags::GRAPHICS | QueueFlags::COMPUTE)
+                if family.queue_flags.contains(QueueFlags::GRAPHICS | QueueFlags::COMPUTE) {
+                    Some(i as u32)
+                } else {
+                    None
+                }
             })
             .ok_or(NoAcceptableQueueFamily)
+    }
 
-        /*
-        let queue_create_info = DeviceQueueCreateInfo::builder().build();
+    // If presentation to "surface" is supported on the queue, presentation_queue must be equal to queue.
+    // If not, a second queue must be provided in presentation_queue and presentation_queue_index.
+    // If surface is not VK_NULL_HANDLE, the instance from frontend will have been created with supported for
+    // VK_KHR_surface extension.
+    fn select_queue_families(
+        physical_device: &PhysicalDeviceInfo,
+        surface_fn: &Surface,
+        surface: SurfaceKHR,
+    ) -> Result<QueueFamilies, VulkanNegotiationError> {
+        let single_queue_family = physical_device
+            .queue_families
+            .iter()
+            .enumerate()
+            .find_map(|(i, family)| unsafe {
+                let i = i as u32;
+                // The core must ensure that the queue and queue_family_index support GRAPHICS and COMPUTE.
+                if !family.queue_flags.contains(QueueFlags::GRAPHICS | QueueFlags::COMPUTE) {
+                    return None;
+                }
 
-        let presentation_queue_create_info = DeviceQueueCreateInfo::builder().build();
+                match surface_fn.get_physical_device_surface_support(physical_device.device, i, surface) {
+                    Ok(true) => Some(Ok(QueueFamilies::new(i, i))),
+                    // This queue also supports presentation, so let's use it!
+                    Ok(false) => None,
+                    // This queue doesn't support presentation, let's keep searching.
+                    Err(error) => Some(Err(VulkanError("vkGetPhysicalDeviceSurfaceSupportKHR", error))),
+                    // We have a problem, gotta report it.
+                }
+            })
+            .transpose()?;
 
-        let queues = [queue_create_info, presentation_queue_create_info];
-
-        let device_create_info = DeviceCreateInfo::builder()
-            .enabled_extension_names(required_device_extensions)
-            .enabled_layer_names(required_device_layers)
-            .enabled_features(&required_features)
-            .queue_create_infos(queues.as_slice())
-            .build();
-        // TODO: Only get the first element if we don't need presentation_queue_create_info
-
-        match instance.create_device((*context).gpu, &device_create_info, None) {
-            Ok(device) => device,
-            Err(error) => {
-                error!("Failed to create a VkDevice: {error}");
-                return false;
-            }
+        if let Some(single_queue_family) = single_queue_family {
+            return Ok(single_queue_family);
         }
-        */
+
+        // We couldn't find a single queue that supported graphics, compute, *and* present.
+        // So we'll have to split them up.
+
+        let queue_family = Self::select_queue_family(physical_device)?;
+        // Here's our graphics/compute queue, now for a present queue
+
+        let presentation_queue_family = physical_device
+            .queue_families
+            .iter()
+            .enumerate()
+            .find_map(|(i, properties)| unsafe {
+                match surface_fn.get_physical_device_surface_support(physical_device.device, i as u32, surface) {
+                    Ok(true) => Some(Ok(i as u32)), // This queue family supports presentation, let's use it
+                    Ok(false) => None, // This queue family doesn't support presentation, let's not use it
+                    Err(error) => Some(Err(VulkanError("vkGetPhysicalDeviceSurfaceSupportKHR", error))), // There was an error, let's report it
+                }
+            })
+            .transpose()?;
+
+        if let Some(presentation_queue_family) = presentation_queue_family {
+            // If we found a queue family that supports presentation...
+            Ok(QueueFamilies::new(queue_family, presentation_queue_family))
+        }
+        else {
+            Err(NoAcceptableQueueFamily)
+        }
     }
 }
 
@@ -440,7 +491,6 @@ struct PhysicalDeviceInfo {
     layers: Vec<LayerProperties>,
     queue_families: Vec<QueueFamilyProperties>,
 }
-
 impl PhysicalDeviceInfo {
     pub unsafe fn new(instance: &Instance, device: PhysicalDevice) -> VkResult<PhysicalDeviceInfo> {
         assert_ne!(device, PhysicalDevice::null());
@@ -453,6 +503,21 @@ impl PhysicalDeviceInfo {
             features: instance.get_physical_device_features(device),
             queue_families: instance.get_physical_device_queue_family_properties(device),
         })
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct QueueFamilies {
+    queue_family_index: u32,
+    presentation_queue_family_index: u32,
+}
+
+impl QueueFamilies {
+    pub fn new(queue_family_index: u32, presentation_queue_family_index: u32) -> Self {
+        Self {
+            queue_family_index,
+            presentation_queue_family_index,
+        }
     }
 }
 
