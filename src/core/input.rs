96bb677690dd78cf2d79d55a4cfb75693bdeefbf
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-05-30 11:30:59 -0400

    Adjust some input descriptors

--- src/core/input.rs
+++ src/core/input.rs
@@ -5,15 +5,15 @@ use rust_libretro_sys::retro_input_descriptor;
 use rust_libretro_sys::*;
 
 pub const INPUT_DESCRIPTORS: &[retro_input_descriptor] = &input_descriptors!(
-    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP, "Up" },
-    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN, "Down" },
-    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT, "Left" },
-    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT, "Right" },
-
+    { 0, RETRO_DEVICE_KEYBOARD, 0, RETRO_DEVICE_ID_JOYPAD_UP, "Up Key" },
+    { 0, RETRO_DEVICE_KEYBOARD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN, "Down Key" },
+    { 0, RETRO_DEVICE_KEYBOARD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT, "Left Key" },
+    { 0, RETRO_DEVICE_KEYBOARD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT, "Right Key" },
     { 0, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_LEFT, "Left Mouse Button" },
     { 0, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_RIGHT, "Right Mouse Button" },
 );
 
+// TODO: Add a Keyboard subclass with just the supported keys
 pub const CONTROLLER_DESCRIPTIONS: &[retro_controller_description] = &[
     retro_controller_description {
         desc: c_char_ptr!("Keyboard"),
